2d69a81708e38c79fe50dc7fac6c8b40658b323f
Morgan Herlocker <morgan.herlocker@gmail.com>
2012-08-10 15:31:04 -0400

    got basic bg working

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
-window.onload = function () {
+window.onload = function () 
+{
     //start crafty
     Crafty.init(800, 672);
     //Crafty.canvas.init();
@@ -7,18 +8,20 @@ window.onload = function () {
 	//preload the needed assets
 	Crafty.load(["images/sprite.png", "images/bg.png"], function() {
 		//splice the spritemap
-		Crafty.sprite(
-		);
-
+		Crafty.sprite(16  , "images/sprite.png", {
+			skiRight: [0,0],
+			skiRightDown: [1,0]
+		});
 		//start the main scene when loaded
 		Crafty.scene("main");
 	});
 	
-	Crafty.scene("main", function() {
+	Crafty.scene("main", function() 
+	{
 		Crafty.background("url('images/bg.png')");
 		
-		//main stuff
 		
-	}
+			
+	});
 };
 
