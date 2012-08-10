d2cd82eb6178fba9a71d8fc69b04217379faae34
Morgan Herlocker <morgan.herlocker@gmail.com>
2012-08-10 17:17:25 -0400

    player sprite logic for turning is complete

--- game.js
+++ game.js
@@ -7,13 +7,24 @@ window.onload = function () {
 	Crafty.load(["images/sprite.png", "images/bg.png"], function() {
 		//splice the spritemap
 		Crafty.sprite(38 , "images/sprite.png", {
-			skiDown: [1.75, 0, .4, 1],
-			skiRight: [0, 0, .6, 1],
-			skiRightDown: [1,0]
+			skiLeft: [6.55, 10.8, .59, .75],
+			skiLeftDown: [6, 10.8, .55, .75],
+			skiDown: [1.75, 0, .4, .85],
+			skiRightDown: [1.31, 0, .4, .85],
+			skiRight: [0, 0, .6, .85]
 		});
 		//start the main scene when loaded
 		Crafty.scene("main");
 	});
+	
+	var playerSprites = ["skiLeft",
+			"skiLeftDown",
+			"skiDown",
+			"skiRightDown",
+			"skiRight"];
+	
+	var currentSprite = 2;
+	var lastSprite = 2;
 	
 	Crafty.scene("main", function() 
 	{
@@ -22,20 +33,36 @@ window.onload = function () {
 		
 		//player entity
 		var player = Crafty.e("2D, Canvas, skiDown, Controls, Collision")
-			.attr({move: {left: false, right: false, up: false, down: false}, xspeed: 0, yspeed: 0, decay: 0.98, 
-				x: Crafty.viewport.width / 2, y: Crafty.viewport.height / 2, score: 0})
-			.origin("center")
+			.attr({move: {left: false, right: false}, xspeed: 0, yspeed: 0, decay: 0.9, 
+				x: Crafty.viewport.width / 2, y: Crafty.viewport.height / 10, score: 0})
+			.origin("top")
 			.bind("KeyDown", function(e) {
 				//on keydown, set the move booleans
 				if(e.keyCode === Crafty.keys.RIGHT_ARROW) {
 					this.move.right = true;
+					if (currentSprite < 4) {
+						currentSprite++;
+					}
 				} else if(e.keyCode === Crafty.keys.LEFT_ARROW) {
 					this.move.left = true;
+					if (currentSprite > 0){
+						currentSprite--;
+					}
 				}
 			})
-			.bind("EnterFrame", function() {
-				if(this.move.right) this.rotation += 5;
-				if(this.move.left) this.rotation -= 5;
+			.bind("KeyUp", function(e) {
+				//on key up, set the move booleans to false
+				if(e.keyCode === Crafty.keys.RIGHT_ARROW) {
+					this.move.right = false;
+				} else if(e.keyCode === Crafty.keys.LEFT_ARROW) {
+					this.move.left = false;
+				}
+			})
+			.bind("EnterFrame", function() {			
+				if (currentSprite != lastSprite){
+					this.removeComponent(playerSprites[lastSprite]).addComponent(playerSprites[currentSprite]);
+					lastSprite = currentSprite;
+				}
 			}); 
 			
 	});
